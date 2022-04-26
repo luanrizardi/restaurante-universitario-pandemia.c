411938d0dbdf92c6040b63969ce2dd2d9fcdb899
LUAN CARLO RIZARDI <lcr20@urquell>
2022-04-26 18:40:53 -0300

    26/04

--- ru_na_pandemia.c
+++ ru_na_pandemia.c
@@ -14,7 +14,7 @@ typedef struct {
 } pessoa_t;
 
 pessoa_t** pessoa_cria (int ticket){
-    int x = 1000;
+    int x = 1050;
     int i;
     pessoa_t **pessoa;
     pessoa = malloc(x * sizeof(pessoa_t*));
@@ -69,43 +69,44 @@ int main(){
     
     // 4
     fila_t* fila = fila_cria();
-    for(i = 0; i < 1000; i++)
+    for(i = 1; i < 1050; i++)
         queue(fila, pessoa[i]->ticket);
 
+    fila_imprime(fila);
     // 5
     lista_t* lista = lista_cria();
     i = 0;
+    printf("%d \n", fila->ini->chave);
     while (!pilha_vazia(pilha_ref) && !fila_vazia(fila)) {
-        if (pessoa[i]->vacina == 0){
+        if (pessoa[fila->ini->chave]->vacina == 0){
             dequeue(fila, &aux);
-            lista_insere_fim(lista, pessoa[i]->ticket);
-            i++;
+            lista_insere_fim(lista, pessoa[fila->ini->chave]->ticket);
         }
-        else if (pessoa[i]->masc == 1) {
-            pessoa[i]->dinheiro = pessoa[i]->dinheiro - 1.30;
+        else if (pessoa[fila->ini->chave]->masc == 1) {
+            pessoa[fila->ini->chave]->dinheiro = pessoa[fila->ini->chave]->dinheiro - 1.30;
             dequeue(fila, &aux);
             pop(pilha_ref);
-            i++;
         }
         else {
             dequeue(fila, &aux);
-            if(pessoa[i]->dinheiro >= 2.50 && !pilha_vazia(pilha_masc)) {
-                pessoa[i]->dinheiro = pessoa[i]->dinheiro - 2.50;
-                pessoa[i]->masc = 1;
-                queue(fila, pessoa[i]->ticket);
-                pop(pilha_masc);
+            if(pessoa[fila->ini->chave]->dinheiro >= 2.50 && !pilha_vazia(pilha_masc)) {
+                pessoa[fila->ini->chave]->dinheiro = pessoa[fila->ini->chave]->dinheiro - 2.50;
+                pessoa[fila->ini->chave]->masc = 1;
+                queue(fila, pessoa[fila->ini->chave]->ticket);
+                printf("ticket: %d\n", pessoa[fila->ini->chave]->ticket);
             }
             else {
-                lista_insere_fim(lista, pessoa[i]->ticket);
-                i++;
+                lista_insere_fim(lista, pessoa[fila->ini->chave]->ticket);
             }
         }
+        printf("%d \n", fila->ini->chave);
+
     }
     fila_destroi(fila);
-    printf("%d \n", lista_tamanho(lista));
-    //lista_imprime(lista);
-    printf("%d \n", pilha_tamanho(pilha_ref));
-    printf("%d \n", pilha_tamanho(pilha_masc));
+    //printf("%d \n", lista_tamanho(lista));
+    // lista_imprime(lista);
+    //printf("%d \n", pilha_tamanho(pilha_ref));
+    //printf("%d \n", pilha_tamanho(pilha_masc));
     
     return 0;
 }
